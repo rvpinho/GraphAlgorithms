79b8b8091debd54424ac6f7a3132a8ec0c5e8443
rvpinho <rafael.pinho@aluno.ufop.edu.br>
2022-08-10 19:15:21 -0300

    professor corrections aplied

--- src/App.java
+++ src/App.java
@@ -1,13 +1,16 @@
 class Main {
-    public static void main(String[] args) {
-      Graph g1 = new Graph(4);
-      g1.addEdge(0, 1, 3);
-      g1.addEdge(1, 0, 3);
-      g1.addEdge(0, 3, 4);
-      g1.addEdge(3, 0, 4);
-      g1.addEdge(3, 4, 4); //warning
-  
-      System.out.println(g1);
-  
-    }
+  public static void main(String[] args) {
+    Graph g1 = new Graph(4);
+    g1.addEdge(0, 1, 3);
+    g1.addEdge(1, 0, 3);
+    g1.addEdge(0, 3, 4);
+    g1.addEdge(3, 0, 4);
+    g1.addEdge(3, 4, 4); //Warning
+    
+    System.out.println(g1);
+    System.out.println(g1.degree(0));
+    System.out.println(g1.degree(1));
+    System.out.println(g1.degree(2));
+    System.out.println(g1.degree(3));
   }
+}
