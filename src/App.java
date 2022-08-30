a5fbc2d1d1d2f03cdbb76c97e97d2437c4be476f
rvpinho <rafael.pinho@aluno.ufop.edu.br>
2022-08-29 23:44:31 -0300

    update aula 28/08

--- src/App.java
+++ src/App.java
@@ -1,12 +1,34 @@
+import java.io.IOException;
+
+
+
+
 class Main {
   public static void main(String[] args) {
     Graph g1 = new Graph(4);
+  /* 
     g1.addEdge(0, 1, 3);
     g1.addEdge(1, 0, 3);
     g1.addEdge(0, 3, 4);
     g1.addEdge(3, 0, 4);
     g1.addEdge(3, 4, 4); //Warning
+    */
 
+    g1.addEdgeUnoriented(7, 5, 1);
+    g1.addEdgeUnoriented(7, 1, 1);
+    g1.addEdgeUnoriented(7, 2, 1);
+    g1.addEdgeUnoriented(1, 0, 1);
+    g1.addEdgeUnoriented(1, 4, 1);
+    g1.addEdgeUnoriented(2, 3, 1);
+    g1.addEdgeUnoriented(5, 6, 1);
+    g1.addEdgeUnoriented(6, 8, 1);
+    System.out.println(g1.bfs(7));
+    System.out.println(g1.connected());
+
+    Graph g2 = new Graph("grafo1.txt");
+    System.out.println(g2);
+
+    /*/
     System.out.println(g1);
     System.out.println(g1.degree(0));
     System.out.println(g1.degree(1));
@@ -16,6 +38,25 @@ class Main {
     System.out.println(g1.highestDegree()); //2
     System.out.println(g1.lowestDegree()); //0
     System.out.println(g1.complementGraph());
+*/
+
+// Graph g1 = new Graph(4);
+    // System.out.println(g1.toString());
+    // g1.addEdge(0, 1, 3);
+    // g1.addEdge(1, 0, 3);
+    // g1.addEdge(0, 3, 4);
+    // g1.addEdge(3, 0, 4);
+    // g1.addEdge(3, 4, 2); // warning
+    // System.out.println(g1);
+    // System.out.println(g1.degree(0)); // 2
+    // System.out.println(g1.degree(1)); // 1
+    // System.out.println(g1.degree(2)); // 0
+    // System.out.println(g1.degree(3)); // 1
+    // System.out.println(g1.highestDegree()); // 2
+    // System.out.println(g1.lowestDegree()); // 0
+    // System.out.println(g1.complement());
+    // System.out.println(g1.subGraph());    
+
 
   }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Graph {
 
   private int countNodes;
@@ -97,10 +103,105 @@ public class Graph {
     return complementGraph;
   }
 
+  public Graph complementGraph() {
+    Graph g2 = new Graph(this.countNodes);
+    for(int i=0; i<this.adjMatrix.length; i++) {
+      for(int j=0; j<this.adjMatrix[i].length; j++) {
+        if(i != j && this.adjMatrix[i][j] == 0) {
+          g2.addEdge(i, j, i);
+        }
+      }
+    }
+    return g2;
+  }
 
+  public float density() {
+    // d = E / V * V - 1
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+  }
 
+  public boolean subgraph(Graph g2) {
+    // retorna true se g2 for subgrafo de this;
+    // retorna false caso contrario
+    if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+      return false;
+    for (int i = 0; i < g2.adjMatrix.length; ++i) {
+      for (int j = 0; j < g2.adjMatrix[i].length; ++j) {
+        if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0)
+          return false;
+      }
+    }
+    return true;
+  }
 
+  public ArrayList<Integer> bfs(int s) { // busca em largura
+    // inicialização
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    // Loop principal
+    while(Q.size() > 0) {
+      int u = Q.remove(0);
+      for(int v=0; v<this.adjMatrix[u].length; v++) {
+        if(this.adjMatrix[u][v] != 0) { // v é adjacente a u
+          if(desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
+        }
+      }
+    }
+    return R;
+  }
 
+  public ArrayList<Integer> dfs(int s) { // busca em profundidade por último valor
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> S = new ArrayList();
+    S.add(s);
+    ArrayList<Integer> R = new ArrayList();
+    R.add(s);
+    desc[s] = 1;
+    // loop principal
+    while (S.size() > 0) {
+      int u = S.get(S.size()-1);
+      boolean unstack = true; // desempilha
+      for(int v = 0; v < adjMatrix[u].length;++v){
+        if(this.adjMatrix[u][v] != 0 && desc[v] == 0){
+            S.add(v);
+            R.add(v);
+            desc[v] = 1;
+            unstack = false;
+            break;
+          }
+      } if(unstack)
+          S.remove(S.size()-1);
+    }
+    return R;
+  }
 
+  public boolean connected() {
+    return this.bfs(0).size() < this.countNodes;
+  }
 
+  public ArrayList<Integer> dfsRec(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> R = new ArrayList<>();
+    dfsRecursiveAux(s, desc, R);
+    return R;
+  }
+  
+  public void dfsRecursiveAux(int u, int[] desc, ArrayList<Integer> R) {
+    desc[u] = 1;
+    R.add(u);
+    for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+      if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+        dfsRecursiveAux(v, desc, R);
+      }
+    }
+  }  
+  
 }
